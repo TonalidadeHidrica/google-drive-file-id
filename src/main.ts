700cc7bd31c43f48ff07481720d1ad4ccac1d43b
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-01-26 02:49:31 +0900

    Succesfully append elements

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,65 @@
 import { XT } from "./xt";
 
+const ORIGINAL_TAG_KEY = "data-gfdi";
+const ORIGINAL_TAG_VALUE = "1";
+
 const addChild = (parentDiv: HTMLDivElement) => {
-  const div = parentDiv.querySelector("div.KL4NAf");
-  div?.prepend(
-    XT(document, [["a", [{ href: "https://google.com" }, "ID"]]])
+  // const div = parentDiv.querySelector("div.KL4NAf");
+  parentDiv?.prepend(
+    XT(document, [
+      [
+        "a",
+        [
+          {
+            href: "https://google.com",
+            [ORIGINAL_TAG_KEY]: ORIGINAL_TAG_VALUE,
+            style: "padding-right: 5px",
+          },
+          "ID",
+        ],
+      ],
+    ])
   );
-  console.log("after: ", parentDiv.outerHTML);
+  // console.log("after: ", parentDiv.outerHTML);
 };
 
 const nodeObserver = new MutationObserver((records) => {
-  const record = records[0];
-  if (record.removedNodes.length) {
-    // console.log("Oh no, removed!", record.removedNodes);
-    // console.log("target: ", record.target);
-    addChild(<HTMLDivElement>record.target);
+  for (const record of records) {
+    for (const removed of record.removedNodes) {
+      if (
+        removed instanceof HTMLElement &&
+        removed.attributes.getNamedItem(ORIGINAL_TAG_KEY)?.value ===
+          ORIGINAL_TAG_VALUE
+      ) {
+        addChild(<HTMLDivElement>record.target);
+      }
+    }
   }
 });
 
+const inspectDescendants = (node: HTMLElement) => {
+  for (const div of node.querySelectorAll(`div[jsmodel="aDmR9e"]`)) {
+    // console.log(div);
+    const id = div.attributes.getNamedItem("data-id")?.value;
+    if (!id) continue;
+    const parentDiv = div.querySelector("div.KL4NAf");
+    if (!parentDiv) continue;
+
+    nodeObserver.observe(parentDiv, {
+      subtree: true,
+      childList: true,
+    });
+
+    addChild(parentDiv);
+  }
+};
+
 const childObserver = new MutationObserver((records) => {
+  // console.log(records);
   for (const record of records) {
     for (const node of record.addedNodes) {
       if (node instanceof HTMLElement) {
-        for (const div of node.querySelectorAll(`div[jsmodel="aDmR9e"]`)) {
-          const id = div.attributes.getNamedItem("data-id")?.value;
-          if (!id) continue;
-          const parentDiv = div.querySelector("div.uXB7xe");
-          if (!parentDiv) continue;
-          console.log("before: ", parentDiv.outerHTML);
-
-          nodeObserver.observe(parentDiv, {
-            subtree: true,
-            childList: true,
-          });
-
-          addChild(parentDiv);
-
-          return;
-        }
+        inspectDescendants(node);
       }
     }
   }
@@ -50,6 +73,8 @@ const toplevelObserver = new MutationObserver((records) => {
           node.attributes.getNamedItem("guidedhelpid")?.value ==
           "main_container"
         ) {
+          // console.log(node);
+          inspectDescendants(node);
           childObserver.observe(node, {
             subtree: true,
             childList: true,
