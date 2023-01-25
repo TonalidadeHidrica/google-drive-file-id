6416e07dc8afc70bb077bdcbf57b47c479bad721
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-01-26 02:00:17 +0900

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { XT } from "./xt";
 
+const addChild = (parentDiv: HTMLDivElement) => {
+  const div = parentDiv.querySelector("div.KL4NAf");
+  div?.prepend(
+    XT(document, [["a", [{ href: "https://google.com" }, "ID"]]])
+  );
+  console.log("after: ", parentDiv.outerHTML);
+};
+
+const nodeObserver = new MutationObserver((records) => {
+  const record = records[0];
+  if (record.removedNodes.length) {
+    // console.log("Oh no, removed!", record.removedNodes);
+    // console.log("target: ", record.target);
+    addChild(<HTMLDivElement>record.target);
+  }
+});
+
 const childObserver = new MutationObserver((records) => {
   for (const record of records) {
     for (const node of record.addedNodes) {
@@ -9,13 +26,16 @@ const childObserver = new MutationObserver((records) => {
           if (!id) continue;
           const parentDiv = div.querySelector("div.uXB7xe");
           if (!parentDiv) continue;
-          console.log("before: ", parentDiv);
-          parentDiv.appendChild(
-            document.createElement("a")
-            // XT(document, [["a", [{ href: "https://google.com" }, "ID"]]])
-          );
-          console.log("after: ", parentDiv);
-          console.log("Refreshed");
+          console.log("before: ", parentDiv.outerHTML);
+
+          nodeObserver.observe(parentDiv, {
+            subtree: true,
+            childList: true,
+          });
+
+          addChild(parentDiv);
+
+          return;
         }
       }
     }
@@ -47,3 +67,12 @@ if (elem !== null) {
     childList: true,
   });
 }
+
+// setTimeout(() => {
+//   const elem = document.querySelector("div.uXB7xe");
+//   // console.log(elem?.outerHTML);
+//   const child = document.createElement("a");
+//   child.innerText = "Hello world!";
+//   elem?.appendChild(child);
+//   // console.log(elem?.outerHTML);
+// }, 5000);
