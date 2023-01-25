cfda408950154fbcd9568c40ac2285c1a98efec4
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-01-26 03:41:09 +0900

    Clean up code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import {XT} from "./xt";
+import { XT } from "./xt";
 
 const ORIGINAL_TAG_KEY = "data-gfdi";
 const ORIGINAL_TAG_VALUE = "1";
 const FILE_ID_KEY = "data-id";
 
 const addChild = (parentDiv: HTMLDivElement, fileId: string) => {
-  // const div = parentDiv.querySelector("div.KL4NAf");
   parentDiv?.prepend(
     XT(document, [
       [
@@ -15,7 +14,7 @@ const addChild = (parentDiv: HTMLDivElement, fileId: string) => {
             href: "#",
             [ORIGINAL_TAG_KEY]: ORIGINAL_TAG_VALUE,
             [FILE_ID_KEY]: fileId,
-            class: "gfdi-link"
+            class: "gfdi-link",
           },
           "ID",
         ],
@@ -26,7 +25,6 @@ const addChild = (parentDiv: HTMLDivElement, fileId: string) => {
       ],
     ])
   );
-  // console.log("after: ", parentDiv.outerHTML);
 };
 
 const nodeObserver = new MutationObserver((records) => {
@@ -35,7 +33,7 @@ const nodeObserver = new MutationObserver((records) => {
       if (
         removed instanceof HTMLElement &&
         removed.attributes.getNamedItem(ORIGINAL_TAG_KEY)?.value ===
-        ORIGINAL_TAG_VALUE
+          ORIGINAL_TAG_VALUE
       ) {
         const fileId = removed.attributes.getNamedItem(FILE_ID_KEY)!.value;
         addChild(<HTMLDivElement>record.target, fileId);
@@ -46,7 +44,6 @@ const nodeObserver = new MutationObserver((records) => {
 
 const inspectDescendants = (node: HTMLElement) => {
   for (const div of node.querySelectorAll(`div[jsmodel="aDmR9e"]`)) {
-    // console.log(div);
     const fileId = div.attributes.getNamedItem("data-id")?.value;
     if (!fileId) continue;
     const parentDiv = div.querySelector("div.KL4NAf");
@@ -62,7 +59,6 @@ const inspectDescendants = (node: HTMLElement) => {
 };
 
 const childObserver = new MutationObserver((records) => {
-  // console.log(records);
   for (const record of records) {
     for (const node of record.addedNodes) {
       if (node instanceof HTMLElement) {
@@ -80,7 +76,6 @@ const toplevelObserver = new MutationObserver((records) => {
           node.attributes.getNamedItem("guidedhelpid")?.value ==
           "main_container"
         ) {
-          // console.log(node);
           inspectDescendants(node);
           childObserver.observe(node, {
             subtree: true,
@@ -100,17 +95,17 @@ if (elem !== null) {
   });
 }
 
-document.querySelector("head")?.append(XT(document, [["style", [`
+document.querySelector("head")?.append(
+  XT(document, [
+    [
+      "style",
+      [
+        `
   a.gfdi-link {
     padding-right: 5px;
   }
-`]]]));
-
-// setTimeout(() => {
-//   const elem = document.querySelector("div.uXB7xe");
-//   // console.log(elem?.outerHTML);
-//   const child = document.createElement("a");
-//   child.innerText = "Hello world!";
-//   elem?.appendChild(child);
-//   // console.log(elem?.outerHTML);
-// }, 5000);
+`,
+      ],
+    ],
+  ])
+);
